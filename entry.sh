f229a9ad2b129e1748c6b3beea94ec2cc1f9ff62
noitsjocelyn <303310+noitsjocelyn@users.noreply.github.com>
2022-07-30 16:13:29 -0700

    - Remove redundant env vars from entry template - Fix steamcmd warning "use force_install_dir before logon"

--- entry.sh
+++ entry.sh
@@ -1,10 +1,6 @@
-STEAMAPPID=1690800
-STEAMAPP=Satisfactory
-STEAMAPPDIR="${HOMEDIR}/${STEAMAPP}-dedicated"
-
 export LD_LIBRARY_PATH=${STEAMAPPDIR}/linux64:$LD_LIBRARY_PATH
 
-bash ${STEAMCMDDIR}/steamcmd.sh +login anonymous +force_install_dir ${STEAMAPPDIR} +app_update ${STEAMAPPID} validate +quit
+bash ${STEAMCMDDIR}/steamcmd.sh +force_install_dir ${STEAMAPPDIR} +login anonymous +app_update ${STEAMAPPID} validate +quit
 test -d ~/.config/Epic/FactoryGame/Saved || mkdir -p ~/.config/Epic/FactoryGame/Saved
 test -d ${STEAMAPPDIR}/saves || mkdir ${STEAMAPPDIR}/saves
 test -L ~/.config/Epic/FactoryGame/Saved/SaveGames || ln -s ${STEAMAPPDIR}/saves ~/.config/Epic/FactoryGame/Saved/SaveGames
